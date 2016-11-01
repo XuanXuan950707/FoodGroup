dc4563411e6e547e128d569a4133ef892fadd408
郭轩宇 <1013355265@qq.com>
2016-11-01 19:56:33 +0800

    no message

--- app/src/main/java/com/example/dllo/foodgroup/strolleat/homepage/HomepageAdapter.java
+++ app/src/main/java/com/example/dllo/foodgroup/strolleat/homepage/HomepageAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.example.dllo.foodgroup.Bean.HomepageItemBean;
 import com.example.dllo.foodgroup.R;
+import com.example.dllo.foodgroup.base.BaseViewHolder;
 import com.example.dllo.foodgroup.tools.CirclemageView;
 import com.example.dllo.foodgroup.tools.VolleySingleton;
 
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 /**
  * Created by dllo on 16/10/27.
  */
-public class HomepageAdapter extends RecyclerView.Adapter<HomepageAdapter.MyViewHolder> {
+public class HomepageAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     private Context context;
     private ArrayList<HomepageItemBean> arrayList;
 
@@ -47,37 +48,45 @@ public class HomepageAdapter extends RecyclerView.Adapter<HomepageAdapter.MyView
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == 1) {
-            View view = LayoutInflater.from(context).inflate
-                    (R.layout.homepage_item, parent, false);
-            MyViewHolder myViewHolder = new MyViewHolder(view);
-            return myViewHolder;
+//            View view = LayoutInflater.from(context).inflate
+//                    (R.layout.homepage_item, parent, false);
+//            MyViewHolder myViewHolder = new MyViewHolder(view);
+            return BaseViewHolder.getViewHolder(parent,R.layout.homepage_item);
         }else if(viewType == 2){
-            View view1 = LayoutInflater.from(context).inflate
-                    (R.layout.homepage_shortitem,parent,false);
-            MyViewHolder myViewHolder = new MyViewHolder(view1);
-            return myViewHolder;
+//            View view1 = LayoutInflater.from(context).inflate
+//                    (R.layout.homepage_shortitem,parent,false);
+//            MyViewHolder myViewHolder = new MyViewHolder(view1);
+            return BaseViewHolder.getViewHolder(parent,R.layout.homepage_shortitem);
         }
         return null;
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
 
         if (arrayList.get(position).getTitle() != null) {
-            VolleySingleton.getInstance().getImage
-                    (arrayList.get(position).getCardImage(), holder.cardImage);
-            VolleySingleton.getInstance().getImage
-                    (arrayList.get(position).getPublisherImage(), holder.publisherImage);
-            holder.title.setText(arrayList.get(position).getTitle());
+//            VolleySingleton.getInstance().getImage
+//                    (, holder.cardImage);
+            holder.setImage(R.id.homepage_item_cardimage,arrayList.get(position).getCardImage());
+//            VolleySingleton.getInstance().getImage
+//                    (, holder.publisherImage);
+            holder.setImage(R.id.homepage_item_publisherimage,
+                    arrayList.get(position).getPublisherImage());
+//            holder.title.setText();
+            holder.setText(R.id.homepage_item_title,arrayList.get(position).getTitle());
 //        Log.d("response", arrayList.get(position).getTitle());
-            holder.likeCt.setText(arrayList.get(position).getLikeCt());
-            holder.publisher.setText(arrayList.get(position).getPublisher());
-            holder.description.setText(arrayList.get(position).getDescription());
+//            holder.likeCt.setText();
+            holder.setText(R.id.homepage_item_likect,arrayList.get(position).getLikeCt());
+//            holder.publisher.setText();
+            holder.setText(R.id.homepage_item_publisher,arrayList.get(position).getPublisher());
+//            holder.description.setText();
+            holder.setText(R.id.homepage_item_description,arrayList.get(position).getDescription());
         }else if(arrayList.get(position).getTitle() == null) {
-            VolleySingleton.getInstance().getImage
-                    (arrayList.get(position).getCardImage(),holder.shortCardImage);
+//            VolleySingleton.getInstance().getImage
+//                    (,holder.shortCardImage);
+            holder.setImage(R.id.homepage_shortitem_iv,arrayList.get(position).getCardImage());
         }
     }
 
@@ -85,27 +94,27 @@ public class HomepageAdapter extends RecyclerView.Adapter<HomepageAdapter.MyView
     public int getItemCount() {
         return arrayList == null? 0 : arrayList.size() ;
     }
-    public class MyViewHolder extends RecyclerView.ViewHolder{
-
-        private final ImageView cardImage;
-        private final TextView title;
-
-        private final TextView likeCt;
-        private final TextView publisher;
-        private final TextView description;
-        private final CirclemageView publisherImage;
-        private final ImageView shortCardImage;
-
-
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            cardImage = (ImageView) itemView.findViewById(R.id.homepage_item_cardimage);
-            title = (TextView) itemView.findViewById(R.id.homepage_item_title);
-            publisherImage = (CirclemageView) itemView.findViewById(R.id.homepage_item_publisherimage);
-            likeCt = (TextView) itemView.findViewById(R.id.homepage_item_likect);
-            publisher = (TextView) itemView.findViewById(R.id.homepage_item_publisher);
-            description = (TextView) itemView.findViewById(R.id.homepage_item_description);
-            shortCardImage = (ImageView) itemView.findViewById(R.id.homepage_shortitem_iv);
-        }
-    }
+//    public class MyViewHolder extends RecyclerView.ViewHolder{
+//
+//        private final ImageView cardImage;
+//        private final TextView title;
+//
+//        private final TextView likeCt;
+//        private final TextView publisher;
+//        private final TextView description;
+//        private final CirclemageView publisherImage;
+//        private final ImageView shortCardImage;
+//
+//
+//        public MyViewHolder(View itemView) {
+//            super(itemView);
+//            cardImage = (ImageView) itemView.findViewById(R.id.homepage_item_cardimage);
+//            title = (TextView) itemView.findViewById(R.id.homepage_item_title);
+//            publisherImage = (CirclemageView) itemView.findViewById(R.id.homepage_item_publisherimage);
+//            likeCt = (TextView) itemView.findViewById(R.id.homepage_item_likect);
+//            publisher = (TextView) itemView.findViewById(R.id.homepage_item_publisher);
+//            description = (TextView) itemView.findViewById(R.id.homepage_item_description);
+//            shortCardImage = (ImageView) itemView.findViewById(R.id.homepage_shortitem_iv);
+//        }
+//    }
 }
