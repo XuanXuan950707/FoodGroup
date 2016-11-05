4306f66ba13355fd2b1e31828dc506d2065bd0de
郭轩宇 <1013355265@qq.com>
2016-11-05 19:09:11 +0800

    no message

---
+++ app/src/main/java/com/example/dllo/foodgroup/main/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.dllo.foodgroup.main;
+
+import com.example.dllo.foodgroup.R;
+import com.example.dllo.foodgroup.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/11/5.
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void iniData() {
+
+    }
+}
